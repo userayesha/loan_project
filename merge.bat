9a483804cef19a627d2e0467a81c3b08db735199
userayesha <45415971+userayesha@users.noreply.github.com>
2020-03-21 15:56:36 +0530

    ading for git

---
+++ merge.bat
@@ -0,0 +1,7 @@
+git config --global user.name "userayesha"
+git config --global user.email "ayesha.aws@gmail.com"
+git  checkout release
+git fetch origin
+git pull
+git merge origin/develope
+git push
